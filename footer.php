c067b645bbbfc2fac3dac5c8f3e36334c288194f
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-01-20 16:37:02 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,53 @@
             </div>
+            <!-- !container -->
         </main>
-        <!-- !main -->  
+        <!-- !main --> 
+
+        <!-- footer -->
+        <footer class="bg-primary text-white">
+            <div class="container pt-4">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <h6>About</h6>
+                        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, facere excepturi? Atque nulla eligendi quisquam hic quia reiciendis? Voluptatibus excepturi provident id ab recusandae neque, alias atque earum cupiditate aliquam debitis ut mollitia sunt sed, totam aperiam accusantium, doloremque eaque quibusdam. Neque assumenda eos corrupti unde laboriosam nisi, dolore accusantium.</p>
+                    </div>
+
+                    <div class="col-xs-6 col-md-3">
+                        <h6>Social</h6>
+                        <ul>
+                            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+                        </ul>
+                    </div>
+
+                    <div class="col-xs-6 col-md-3">
+                        <h6>Quick Links</h6>
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="forum.php">Forum</a></li>
+                            <li><a href="blog.php">Blog</a></li>
+                            <li><a href="login.php">Login</a></li>
+                            <li><a href="register.php">Register</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9 col-sm-6">
+                        <p>
+                            Copyright &copy; <?php echo date('Y'); ?> All Rights Reserved by <a href="#">Mateja Tasic</a>.
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <a href="admin_login">Admin Login</a>
+                    </div>
+                </div>
+            </div>
+        </footer> 
+        <!-- !footer --> 
     </body>
 </html>
