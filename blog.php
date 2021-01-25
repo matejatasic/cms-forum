539c41bf6f40f118d191273fb11d738655de9ede
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-01-25 11:05:35 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,71 @@
+<?php
+    include('./header.php');
+?>
+<div class="row pb-3">
+    <a href="index.php" class="btn btn-primary float-left">Back</a>
+</div>
+<div class="row my-5">
+    <div class="col-md-12">
+        <h2>Blog Posts</h2>
+    </div>
+    <?php
+        if(isset($_SESSION['user_id']) || isset($_SESSION['admin_id'])) {
+            echo '
+                <div class="col-md-12 mb-3">
+                    <a href="add_blog_post.php" class="btn btn-primary float-right">Add post</a>
+                </div>  
+            ';
+        }
+    ?>
+    <div class="col-md-12">
+        <div class="container mt-2" id="posts-container">
+            <div class="row">
+                <?php
+                    $cms->data = array(
+                        ':approved' => 1,
+                    );
+                    $cms->query = 'SELECT * FROM blog_table WHERE approved = :approved';
+                    $total_rows = $cms->total_rows();
+
+                    if($total_rows === 0) {
+                        echo '
+                            <div class="col-md-4">
+                                <p class="font-1">No posts have been added</p>
+                            </div>
+                        ';
+                    }
+                    else {
+                        $result = $cms->result();
+
+                        foreach($result as $row) {
+                            $time = $cms->timePassed($row['created_on']);
+                            $difference = $time[0];
+                            $str_interval = $time[1] !== 'now' ? $difference . ' ' . $time[1] . ' ago' : 'now';
+                            echo '
+                                <div class="col-md-4 mb-3">
+                                    <div class="card h-100">
+                                        <div class="card-header embed-responsive embed-responsive-16by9">
+                                            <img src="'.$row['post_image'].'" alt="news_image" class="card-img-top embed-responsive-item">
+                                        </div>
+                                        <div class="card-body font-2 text-left">
+                                            <h3><a href="#">'.$row['post_title'].'</a></h3>
+                                            <p>'.$cms->shortenText($row['post_body'], 200).'</p>
+                                            
+                                            <div class="card-footer">
+                                                <p>Post by '.$row['post_author'].'</p>
+                                                <p>'.date('F d, Y', strtotime($row['created_on'])).'</p>
+                                            </div> 
+                                        </div>
+                                    </div>
+                                </div>
+                            ';
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include('./footer.php');
+?>
