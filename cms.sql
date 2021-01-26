26ebb929075a8ef9a2120c13ed17af887a9ec475
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-01-26 09:21:52 +0100

    Update cms.sql

--- cms.sql
+++ cms.sql
@@ -1,49 +1,137 @@
-IF cms('cms') IS NULL 
-    CREATE DATABASE dbname
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 26, 2021 at 09:19 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
 
-GO
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `cms`.`users_table` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_table`
+--
+
+CREATE TABLE `admin_table` (
+  `id` int(11) NOT NULL,
+  `admin_email` varchar(100) NOT NULL,
+  `admin_password` varchar(100) NOT NULL,
+  `admin_username` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `admin_image` varchar(100) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `blog_table`
+--
+
+CREATE TABLE `blog_table` (
+  `id` int(11) NOT NULL,
+  `post_title` varchar(100) NOT NULL,
+  `post_body` varchar(255) NOT NULL,
+  `post_author` varchar(50) NOT NULL,
+  `post_image` varchar(150) NOT NULL,
+  `approved` enum('0','1') NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `news_table`
+--
+
+CREATE TABLE `news_table` (
+  `id` int(11) NOT NULL,
+  `news_title` varchar(100) NOT NULL,
+  `news_body` varchar(255) NOT NULL,
+  `news_author` varchar(50) NOT NULL,
+  `news_image` varchar(150) NOT NULL,
+  `approved` enum('0','1') NOT NULL DEFAULT '0',
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `users_table`
+--
+
+CREATE TABLE `users_table` (
+  `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `gender` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
   `user_image` varchar(100) NOT NULL,
-  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `cms`.`news_table` ( 
-  `id` INT NOT NULL AUTO_INCREMENT , 
-  `news_title` VARCHAR(100) NOT NULL ,
-  `news_body` VARCHAR(255) NOT NULL 
-  `news_author` VARCHAR(50) NOT NULL ,
-  `news_image` VARCHAR(150) NOT NULL ,
-  `approved` ENUM('0', '1') NOT NULL DEFAULT '0' , 
-  `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+--
+-- Indexes for table `admin_table`
+--
+ALTER TABLE `admin_table`
+  ADD PRIMARY KEY (`id`);
 
-CREATE TABLE `cms`.`admin_table` ( 
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `admin_email` VARCHAR(100) ,
-  `admin_password` INT(100) NOT NULL , 
-  `admin_username` VARCHAR(50) NOT NULL ,
-  `gender` VARCHAR(50) ,
-  `admin_image` VARCHAR(100) ,  
-  `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+--
+-- Indexes for table `blog_table`
+--
+ALTER TABLE `blog_table`
+  ADD PRIMARY KEY (`id`);
 
-CREATE TABLE `cms`.`blog_table` ( 
-  `id` INT NOT NULL AUTO_INCREMENT , 
-  `post_title` VARCHAR(100) NOT NULL , 
-  `post_body` VARCHAR(255) NOT NULL , 
-  `post_author` VARCHAR(50) NOT NULL , 
-  `post_image` VARCHAR(150) NOT NULL , 
-  `approved` ENUM('0', '1') NOT NULL DEFAULT '0' , 
-  `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+--
+-- Indexes for table `news_table`
+--
+ALTER TABLE `news_table`
+  ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `users_table`
+--
+ALTER TABLE `users_table`
+  ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_table`
+--
+ALTER TABLE `admin_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `blog_table`
+--
+ALTER TABLE `blog_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `news_table`
+--
+ALTER TABLE `news_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users_table`
+--
+ALTER TABLE `users_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
