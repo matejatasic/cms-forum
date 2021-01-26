0d4f32572190045f077ca81f1c3d332aecdcc37b
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-01-26 11:15:23 +0100

    Update add_blog.php

--- add_blog.php
+++ add_blog.php
@@ -90,7 +90,7 @@
                     error: (xhr, ajaxOptions, thrownError) => {
                         console.log(xhr.status);
                         console.log(thrownError);
-                        message.html('<div class="alert alert-danger">There was an error while trying to register...</div>');
+                        message.html('<div class="alert alert-danger">There was an error while trying to add a post...</div>');
                     },
                 });
             }
