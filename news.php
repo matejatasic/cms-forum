d65157a3dc8e1217f17e4397de3de8e9c9dd42ee
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-01-29 13:25:59 +0100

    Add files via upload

---
+++ news.php
@@ -0,0 +1,41 @@
+<?php
+    include('./header.php');
+
+    $id = $_GET['id'];
+
+    $cms->data = array(
+        ':id' => $id,
+    );
+
+    $cms->query = 'SELECT * FROM news_table WHERE id = :id';
+
+    $total_rows = $cms->total_rows();
+
+    //Redirect user if inserted wrong id in the url
+    if($total_rows === 0) {
+        $cms->redirect('index.php');
+    }
+
+    $result = $cms->result();
+
+    $result = $result[0];
+?>
+<div class="row pb-3">
+    <a href="index.php" class="btn btn-primary float-left">Back</a>
+</div>
+<div class="row my-5">
+    <div class="col-md-12">
+        <h2><?php echo $result['news_title']; ?></h2>
+    </div>
+    <div class="col-md-12">
+        <div class="container mt-2 p-5" id="posts-container">
+            <div class="row">
+                <p class="font-2"><?php echo nl2br($result['news_body']); ?></p>
+                <p class="mx-auto mt-3 font-2">News by <?php echo $result['news_author'] . ', ' . $result['created_on']; ?></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include('./footer.php');
+?>
