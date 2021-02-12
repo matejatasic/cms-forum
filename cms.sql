0d8c5932a1d8f2c94901731fb7d78cf9fe99d393
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-02-12 14:29:32 +0100

    Update cms.sql

--- cms.sql
+++ cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2021 at 09:19 AM
+-- Generation Time: Feb 12, 2021 at 02:25 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -37,6 +37,8 @@ CREATE TABLE `admin_table` (
   `created_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `blog_table`
 --
@@ -44,12 +46,72 @@ CREATE TABLE `admin_table` (
 CREATE TABLE `blog_table` (
   `id` int(11) NOT NULL,
   `post_title` varchar(100) NOT NULL,
-  `post_body` varchar(255) NOT NULL,
+  `post_body` text NOT NULL,
   `post_author` varchar(50) NOT NULL,
   `post_image` varchar(150) NOT NULL,
   `approved` enum('0','1') NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments_table`
+--
+
+CREATE TABLE `comments_table` (
+  `id` int(11) NOT NULL,
+  `comment_body` text NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `comment_author` varchar(50) NOT NULL,
+  `approved` enum('0','1') NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_categories`
+--
+
+CREATE TABLE `forum_categories` (
+  `id` int(11) NOT NULL,
+  `cat_title` varchar(255) NOT NULL,
+  `cat_description` varchar(255) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_posts`
+--
+
+CREATE TABLE `forum_posts` (
+  `id` int(11) NOT NULL,
+  `post_content` text NOT NULL,
+  `post_topic` int(11) NOT NULL,
+  `post_author` varchar(255) NOT NULL,
+  `is_admin` enum('0','1') NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_topics`
+--
+
+CREATE TABLE `forum_topics` (
+  `id` int(11) NOT NULL,
+  `topic_title` varchar(255) NOT NULL,
+  `topic_category` int(11) NOT NULL,
+  `topic_author` varchar(50) NOT NULL,
+  `approved` enum('0','1') NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `news_table`
@@ -58,12 +120,14 @@ CREATE TABLE `blog_table` (
 CREATE TABLE `news_table` (
   `id` int(11) NOT NULL,
   `news_title` varchar(100) NOT NULL,
-  `news_body` varchar(255) NOT NULL,
+  `news_body` text NOT NULL,
   `news_author` varchar(50) NOT NULL,
   `news_image` varchar(150) NOT NULL,
   `approved` enum('0','1') NOT NULL DEFAULT '0',
   `created_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users_table`
@@ -80,6 +144,10 @@ CREATE TABLE `users_table` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Indexes for dumped tables
+--
+
+--
 -- Indexes for table `admin_table`
 --
 ALTER TABLE `admin_table`
@@ -89,6 +157,30 @@ ALTER TABLE `admin_table`
 -- Indexes for table `blog_table`
 --
 ALTER TABLE `blog_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments_table`
+--
+ALTER TABLE `comments_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `forum_categories`
+--
+ALTER TABLE `forum_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `forum_posts`
+--
+ALTER TABLE `forum_posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `forum_topics`
+--
+ALTER TABLE `forum_topics`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -118,6 +210,30 @@ ALTER TABLE `admin_table`
 --
 ALTER TABLE `blog_table`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `comments_table`
+--
+ALTER TABLE `comments_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `forum_categories`
+--
+ALTER TABLE `forum_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `forum_posts`
+--
+ALTER TABLE `forum_posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `forum_topics`
+--
+ALTER TABLE `forum_topics`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `news_table`
